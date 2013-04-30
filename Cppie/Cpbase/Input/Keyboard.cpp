c6250f30514a68477598f1037646ea896c0e9136
Anz_MSI <pjc0247@naver.com>
2013-04-30 19:58:21 +0900

    Keyboard 클래스 추가

---
+++ Cppie/Cpbase/Input/Keyboard.cpp
@@ -0,0 +1,44 @@
+#include "Keyboard.h"
+#include "KeyCode.h"
+
+namespace Cppie{
+	Keyboard::Keyboard(){
+		memset(keybuffer, 0, CPPIE_KEYBUFFER_SIZE);
+	}
+	Keyboard::~Keyboard(){
+	}
+
+	int Keyboard::initialize(){
+		return 0;
+	}
+	void Keyboard::dispose(){
+	}
+
+	void Keyboard::update(){
+		memcpy(prevKeybuffer, keybuffer, CPPIE_KEYBUFFER_SIZE);
+
+		memcpy(keybuffer, SDL_GetKeyboardState(NULL), CPPIE_KEYBUFFER_SIZE);
+	}
+
+	bool Keyboard::pressed(int keycode){
+		SDL_Scancode scancode = SDL_GetScancodeFromKey(keycode);
+		if(keybuffer[scancode])
+			return true;
+		else
+			return false;
+	}
+	bool Keyboard::triggered(int keycode){
+		SDL_Scancode scancode = SDL_GetScancodeFromKey(keycode);
+		if(pressed(keycode) && !prevKeybuffer[scancode])
+			return true;
+		else
+			return false;
+	}
+	bool Keyboard::repeated(int keycode){
+		SDL_Scancode scancode = SDL_GetScancodeFromKey(keycode);
+		if(pressed(keycode) && prevKeybuffer[scancode])
+			return true;
+		else
+			return false;
+	}
+}
---
+++ Cppie/Cpbase/Input/Keyboard.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <SDL.h>
+#include <Windows.h>
+#include <gl/GL.h>
+
+#include "../System/Object.h"
+
+#include "KeyCode.h"
+
+#define CPPIE_KEYBUFFER_LENGTH 256
+#define CPPIE_KEYBUFFER_SIZE sizeof(unsigned char)*CPPIE_KEYBUFFER_LENGTH
+
+namespace Cppie{
+	class Keyboard : public Object{
+	private:
+		unsigned char keybuffer[CPPIE_KEYBUFFER_LENGTH];
+		unsigned char prevKeybuffer[CPPIE_KEYBUFFER_LENGTH];
+
+	public:
+
+		Keyboard();
+		~Keyboard();
+
+		virtual int initialize();
+		virtual void dispose();
+
+		virtual void update();
+
+		bool pressed(int keycode);
+		bool triggered(int keycode);
+		bool repeated(int keycode);
+	};
+};
