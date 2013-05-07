254619231a0260c52e3f61b88a87e3270d6c0a08
Anz_MSI <pjc0247@naver.com>
2013-05-07 16:59:36 +0900

    cpPoint, cpColor, cpRect 함수 추가

--- Cppie/Cpbase/Data/make.cpp
+++ Cppie/Cpbase/Data/make.cpp
@@ -5,4 +5,16 @@ namespace Cppie{
 		Size size(w,h);
 		return size;
 	}
+	Rect cpRect(int x,int y,int w,int h){
+		Rect rect(x,y,w,h);
+		return rect;
+	}
+	Point cpPoint(int x,int y){
+		Point point(x,y);
+		return point;
+	}
+	Color cpColor(int r,int g,int b,int a){
+		Color color(r,g,b,a);
+		return color;
+	}
 };
--- Cppie/Cpbase/Data/make.h
+++ Cppie/Cpbase/Data/make.h
@@ -7,4 +7,7 @@
 
 namespace Cppie{
 	Size cpSize(int w,int h);
+	Rect cpRect(int x,int y,int w=1,int h=1);
+	Point cpPoint(int x,int y);
+	Color cpColor(int r,int g,int b,int a=255);
 };
