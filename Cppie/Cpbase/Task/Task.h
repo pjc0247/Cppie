ea86cd6f3742bf52a649bb61d93965a995776124
Anz_MSI <pjc0247@naver.com>
2013-05-11 19:30:22 +0900

    TASK 관련 상수값들 Task.h로 이동

--- Cppie/Cpbase/Task/Task.h
+++ Cppie/Cpbase/Task/Task.h
@@ -2,6 +2,13 @@
 
 #include "../System/Object.h"
 
+
+#define CPPIE_TASK(code)		[&](){code}
+#define CPPIE_SAFE_TASK(code)	[=](){code}
+typedef function<void()>		Task;
+
+#define CPPIE_TASK_IMMEDIATE	0
+
 namespace Cppie{
 
 	void Delay(int ms);
