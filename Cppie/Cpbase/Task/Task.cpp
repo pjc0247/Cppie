3167f89e316f553279c068503427cc1c260f8762
Anz_MSI <pjc0247@naver.com>
2013-05-13 17:05:41 +0900

    TaskObject 추가

--- Cppie/Cpbase/Task/Task.cpp
+++ Cppie/Cpbase/Task/Task.cpp
@@ -2,6 +2,27 @@
 
 namespace Cppie{
 
+	TaskObject::TaskObject(){
+	}
+	TaskObject::TaskObject(Task task){
+		initialize(task);
+	}
+	TaskObject::~TaskObject(){
+		dispose();
+	}
+
+	int TaskObject::initialize(Task task){
+		return 0;
+	}
+	void TaskObject::dispose(){
+		this->task = nullptr;
+	}
+
+	void TaskObject::run(){
+		this->task();
+	}
+
+
 	void Delay(int ms){
 		if(ms <= 0) return;
 		SDL_Delay(ms);
--- Cppie/Cpbase/Task/Task.h
+++ Cppie/Cpbase/Task/Task.h
@@ -2,6 +2,9 @@
 
 #include "../System/Object.h"
 
+#include <functional>
+
+using namespace std;
 
 #define CPPIE_TASK(code)		[&](){code}
 #define CPPIE_SAFE_TASK(code)	[=](){code}
@@ -11,6 +14,20 @@ typedef function<void()>		Task;
 #define CPPIE_TASK_NEXT_LOOP	0
 
 namespace Cppie{
+	class TaskObject : public Object{
+	private:
+		Task task;
+
+	public:
+		TaskObject();
+		TaskObject(Task task);
+		virtual ~TaskObject();
+
+		virtual int initialize(Task task);
+		virtual void dispose();
+
+		virtual void run();
+	};
 
 	void Delay(int ms);
 };
