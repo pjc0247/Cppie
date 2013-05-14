4650d517c6eaf9600963cb4e44c3f7e75a6994b1
Anz_MSI <pjc0247@naver.com>
2013-05-14 13:51:57 +0900

    Task state 추가

--- Cppie/Cpbase/Task/Task.h
+++ Cppie/Cpbase/Task/Task.h
@@ -13,17 +13,18 @@ typedef function<void()>		Task;
 #define CPPIE_TASK_IMMEDIATE	0
 #define CPPIE_TASK_NEXT_LOOP	0
 
-/*
 #define CPPIE_TASKSTATE_DEAD	0	// 작업이 실행되고 있는 상태도 아니고, 실행될 예정도 없는 상태
 #define CPPIE_TASKSTATE_WAITING	1	// 작업이 실행을 대기하고 있는 상태
 #define CPPIE_TASKSTATE_RUNNING	2	// 작업이 실행중인 상태
 #define CPPIE_TASkSTATE_PAUSED	3	// 작업이 일시 중지된 상태
-*/
 
 namespace Cppie{
 	class TaskObject : public Object{
 	protected:
 		Task task;
+	
+	public:
+		int state;
 
 	public:
 		TaskObject();
