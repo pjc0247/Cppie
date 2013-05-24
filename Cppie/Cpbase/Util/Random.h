073331f3629b1801fe29e02ecb7db7987ccf8526
Anz_MSI <pjc0247@naver.com>
2013-05-24 21:57:06 +0900

    CPPIE_TOSS_EVENT 추가

--- Cppie/Cpbase/Util/Random.h
+++ Cppie/Cpbase/Util/Random.h
@@ -1,8 +1,12 @@
 #pragma once
 
-#define CPPIE_TOSS_100(p)	rand()%100<=p ? true : false
-#define CPPIE_TOSS_1000(p)	rand()%100<=p ? true : false
-#define CPPIE_TOSS(p)		CPPIE_TOSS_100(p)
+#define CPPIE_TOSS_100(p)			rand()%100<=p ? true : false
+#define CPPIE_TOSS_1000(p)			rand()%100<=p ? true : false
+#define CPPIE_TOSS(p)				CPPIE_TOSS_100(p)
+
+#define CPPIE_TOSS_EVENT_100(p,e)	CPPIE_TOSS_100(100-p) ? false : e
+#define CPPIE_TOSS_EVENT_1000(p,e)	CPPIE_TOSS_1000(1000-p) ? false : e
+#define CPPIE_TOSS_EVENT(p,e)		CPPIE_TOSS(100-p) ? false : e
 
 namespace Cppie{
 	
