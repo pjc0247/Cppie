b7a448d16d55d44ef7c6d53ef6645a819c89fad8
Anz_MSI <pjc0247@naver.com>
2013-05-24 21:45:14 +0900

    CPPIE_TOSS 추가

---
+++ Cppie/Cpbase/Util/Random.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define CPPIE_TOSS_100(p)	rand()%100<=p ? true : false
+#define CPPIE_TOSS_1000(p)	rand()%100<=p ? true : false
+#define CPPIE_TOSS(p)		CPPIE_TOSS_100(p)
+
+namespace Cppie{
+	
+};
