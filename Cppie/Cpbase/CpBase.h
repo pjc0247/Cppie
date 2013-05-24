b3f92360f2b6b2bb44b8cd09550fda9d3f0371b0
Anz_MSI <pjc0247@naver.com>
2013-05-24 22:15:54 +0900

    주석

--- Cppie/Cpbase/CpBase.h
+++ Cppie/Cpbase/CpBase.h
@@ -1,38 +1,65 @@
 #pragma once
 
+/*
+	Log System
+*/
 #include "Log/Log.h"
 
+/*
+	Cppie System
+*/
 #include "System/Core.h"
 #include "System/SystemManager.h"
 #include "System/Object.h"
 
+/*
+	Data Structures
+*/
 #include "Data/Color.h"
 #include "Data/Point.h"
 #include "Data/Rect.h"
 #include "Data/Size.h"
 #include "Data/make.h"
 
+/*
+	Graphic
+*/
 #include "Graphic/BlendMode.h"
 #include "Graphic/Sprite.h"
 #include "Graphic/SpriteNumber.h"
 #include "Graphic/Graphic.h"
 
+/*
+	Audio
+*/
 #include "Audio/Sound.h"
 #include "Audio/SoundStream.h"
 #include "Audio/SoundManager.h"
 
+/*
+	Scene
+*/
 #include "Scene/Scene.h"
 #include "Scene/SceneManager.h"
 
+/*
+	GameObject interfaces
+*/
 #include "GameObject/ZOrder.h"
 #include "GameObject/Layer.h"
 #include "GameObject/ObjectManager.h"
 #include "GameObject/GameObject.h"
 
+/*
+	Input
+*/
 #include "Input/KeyCode.h"
 #include "Input/Keyboard.h"
 #include "Input/Mouse.h"
 
+/*
+	Task
+*/
 #include "Task/Task.h"
 #include "Task/TaskManager.h"
 #include "Task/AsyncTask.h"
