7933ee6c96d1bc7a6062121fabbdf0b336c859d6
Anz_MSI <pjc0247@naver.com>
2013-05-24 21:37:36 +0900

    RepeatedTask 추가

---
+++ Cppie/Cpbase/Task/RepeatedTask.cpp
@@ -0,0 +1,40 @@
+#include "RepeatedTask.h"
+
+#include "../System/Core.h"
+
+#include "TaskManager.h"
+
+namespace Cppie{
+	RepeatedTask::RepeatedTask(){
+	}
+	RepeatedTask::RepeatedTask(Task task){
+		initialize(task);
+	}
+	RepeatedTask::~RepeatedTask(){
+		dispose();
+	}
+
+	void RepeatedTask::update(){
+		if(this->state != CPPIE_TASKSTATE_WAITING)
+			return;
+
+		if(delay == CPPIE_TASK_NEXT_LOOP){
+			if(startCount != system->loopCount){
+				this->state = CPPIE_TASKSTATE_RUNNING;
+
+				task();
+
+				run(delay);
+			}
+		}
+		else{
+			if(getTicks() - startTick >= delay){
+				this->state = CPPIE_TASKSTATE_RUNNING;
+
+				task();
+				
+				run(delay);
+			}
+		}
+	}
+};
---
+++ Cppie/Cpbase/Task/RepeatedTask.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Task.h"
+#include "DelayedTask.h"
+
+namespace Cppie{
+	class RepeatedTask : public DelayedTask{
+	public:
+		RepeatedTask();
+		RepeatedTask(Task task);
+		virtual ~RepeatedTask();
+
+		virtual void update();
+	};
+};
