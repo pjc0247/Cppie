376e983b568bde18c0e170a4b8a09308c2508d4a
Anz_MSI <pjc0247@naver.com>
2013-06-04 11:36:48 +0900

    BlendMode 수정

--- Cppie/Cpbase/Graphic/BlendMode.h
+++ Cppie/Cpbase/Graphic/BlendMode.h
@@ -2,11 +2,12 @@
 
 #include <SDL.h>
 
+#define CPPIE_BLENDMODE_NONE	SDL_BLENDMODE_NONE
+#define CPPIE_BLENDMODE_ADD		SDL_BLENDMODE_ADD
+#define CPPIE_BLENDMODE_BLEND	SDL_BLENDMODE_BLEND
+#define CPPIE_BLENDMODE_MOD		SDL_BLENDMODE_MOD
+
 namespace Cppie{
 	typedef SDL_BlendMode BlendMode;
 
-	const BlendMode NONE = SDL_BLENDMODE_NONE;
-	const BlendMode BLEND = SDL_BLENDMODE_BLEND;
-	const BlendMode ADD = SDL_BLENDMODE_ADD;
-	const BlendMode MOD = SDL_BLENDMODE_MOD;
 };
