9d93b2d8e4dc56075618563544b6adbe2813b9f1
Anz_MSI <pjc0247@naver.com>
2013-06-04 11:36:54 +0900

    Flip 추가

---
+++ Cppie/Cpbase/Graphic/Flip.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SDL.h>
+
+#define CPPIE_FLIP_NONE			SDL_FLIP_NONE
+#define CPPIE_FLIP_HORIZONTAL	SDL_FLIP_HORIZONTAL
+#define CPPIE_FLIP_VERTICAL		SDL_FLIP_VERTICAL
+
+namespace Cppie{
+	typedef SDL_RendererFlip Flip;
+
+};
