d35eeedf53727950efcf32eceaf4c0e913ff9662
Anz_MSI <pjc0247@naver.com>
2013-06-10 17:40:16 +0900

    AnimatedObject 프로토타�

---
+++ Cppie/Cpbase/GameObject/AnimatedObject.cpp
@@ -0,0 +1,35 @@
+#include "AnimatedObject.h"
+
+#include "../System/Core.h"
+
+namespace Cppie{
+	AnimatedObject::AnimatedObject(){
+	}
+	AnimatedObject::AnimatedObject(float x,float y,Sprite *sprite,int interval){
+		initialize(x,y, sprite, interval);
+	}
+	AnimatedObject::~AnimatedObject(){
+		dispose();
+	}
+
+	int AnimatedObject::initialize(float x,float y,Sprite *sprite,int interval){
+		GameObject::initialize(x,y, sprite);
+
+		task = new RepeatedTask(CPPIE_SAFE_TASK(
+				step ++;
+				if(step >= sprite->wSlice*sprite->hSlice){
+					step = 0;
+				}
+			));
+
+		task->run(interval);
+		
+		return 0;
+	}
+	void AnimatedObject::dispose(){
+	}
+
+	void AnimatedObject::update(){
+		GameObject::draw(x,y);
+	}
+};
---
+++ Cppie/Cpbase/GameObject/AnimatedObject.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "GameObject.h"
+
+#include "../Graphic/Sprite.h"
+#include "../Task/RepeatedTask.h"
+
+namespace Cppie{
+	class AnimatedObject : public GameObject{
+	public:
+		RepeatedTask *animateTask;
+
+	public:
+		AnimatedObject();
+		AnimatedObject(float x,float y,Sprite *sprite,int interval);
+		~AnimatedObject();
+
+		virtual int initialize(float x,float y,Sprite *sprite,int interval);
+		virtual void dispose();
+
+		virtual void update();
+	};
+};
